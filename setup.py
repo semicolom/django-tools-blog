9dc29b7a91403df12eb00c983742a02ca5c005e9
Toni Colom <toni@semicolom.com>
2019-01-11 14:09:57 +0100

    Fixes version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tools-blog',
-    version='0.0.1',
+    version='1.0.1',
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     description='Django app to add blog to your website',
